066450e5658620c0c7a75692c620e054778debe6
Atakan Zengin <atakanzzengin@gmail.com>
2021-12-30 16:31:17 +0100

    Start Middleware section.
    - Initialize logger middleware with the class way by the help of
      `@Injectable()` decorator and `NestMiddleware` interface.
    - Left at
      [here](https://docs.nestjs.com/middleware#middleware-consumer).

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
   imports: [CatsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // We can also set the middleware for all of the methods by providing only the path, instead of an abject.
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+  }
+}
